09ab54a1b46d327f0d93d16e89845280a3495077
Steve Bostock <StevenBostock@tfl.gov.uk>
2024-07-19 21:45:41 +0100

    adding data types

--- Api/Models/Department.cs
+++ Api/Models/Department.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TFL.DevOps.Api.Models;
 
 public class Department
 {
     public int DepartmentID {get;set;}
     public string? Name {get;set;}
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "money")]
     public decimal Budget {get;set;}
     public DateTime StartDate {get;set;}
     public int? InstructorID {get;set;}
